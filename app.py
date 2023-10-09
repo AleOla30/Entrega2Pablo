8e3b0aa6cee4f1789ede0440c9bc19faa7184722
AleOla30 <143103656+AleOla30@users.noreply.github.com>
2023-10-08 20:09:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-
-
 from googletrans import Translator
 from gtts import gTTS
 import os
-import playsound
 
 # Solicita la frase y el idioma de destino
 frase = input("Ingresa la frase que quieres traducir: ")
@@ -15,8 +12,17 @@ traduccion = traductor.translate(frase, dest=idioma_destino)
 frase_traducida = traduccion.text
 
 # Crea una instancia de gTTS para generar el audio
-tts = gTTS(text=frase_traducida, lang=idioma_destino)
-tts.save("temp.wav")
+def text_to_speech(text, tld):
+    
+    tts = gTTS(text,"es", tld, slow=False)
+    try:
+        my_file_name = text[0:20]
+    except:
+        my_file_name = "audio"
+    tts.save(f"temp/{my_file_name}.mp3")
+    return my_file_name, text
+##tts = gTTS(text=frase_traducida, lang=idioma_destino)
+##tts.save("temp.wav")
 
 # Reproduce el archivo de audio usando playsound
 sound_file = "temp.wav"
